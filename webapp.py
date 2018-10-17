44920d08805356f07010c469ed9755934ec79f6f
Jaryd <jarydchandos.5@gmail.com>
2018-10-17 11:52:51 -0700

    Jaryd

--- webapp.py
+++ webapp.py
@@ -17,10 +17,13 @@ def render_page1():
 @app.route("/p2")
 def render_page2():
     return render_template('Page2.html')
-    
+
 @app.route("/p3")
 def render_page3():
   return render_template('Page3.html')
-    
+
+@app.route("/")
+def render_res1():
+    return render_template('Response1.html')
 if __name__=="__main__":
     app.run(debug=False, port=54321)
